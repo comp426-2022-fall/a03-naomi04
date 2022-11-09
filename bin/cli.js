ec8a0f29e072e7d9b6d0bb7f2d7cf0d953f5c9e8
Naomi Smith <naomi04@live.unc.edu>
2022-11-08 20:31:34 -0500

    resolved package issue

--- bin/cli.js
+++ bin/cli.js
@@ -1,24 +1,23 @@
 #!/usr/bin/env node
 
-import { roll } from '/lib/roll.js';
-import minimist from 'minimist';
-import { exit } from 'process';
-
+import { roll } from "../lib/roll.js";
+import minimist from "minimist";
+import { exit } from "process";
 //getting args & passing to roll function
-const args = minimist(process.argv.slice(2))/
+const args = minimist(process.argv.slice(2));
 console.log(args);
 
-const sides = 6;
+var sides = 6;
 if(args.sides) {
     sides = args.sides;
 }
 
-const dice = 2;
+var dice = 2;
 if(args.dice) {
     dice = args.dice;
 }
 
-const rolls = 1;
+var rolls = 1;
 if(args.rolls){
     dice = args.rolls
 }
